d8e7f465c78aa33e7b4d81ccaf94b5a556bef7f7
Jeremy House <jmhouse118@gmail.com>
2025-04-07 21:00:04 -0400

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       {/* <TemperatureProvider> */}
-        <App />
+      <App />
       {/* </TemperatureProvider> */}
     </BrowserRouter>
   </React.StrictMode>
