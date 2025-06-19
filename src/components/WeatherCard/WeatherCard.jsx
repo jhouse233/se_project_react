32f4ca489b6ae4e5cc26e54ea35c345eae54048c
Jeremy House <jmhouse118@gmail.com>
2025-06-19 11:17:07 -0400

    Update WeatherCard

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'; 
+import { React, useState, useEffect } from 'react'; 
 import { useTemperature } from '../../contexts/CurrentTemperatureUnitContext.jsx';
 
 import './WeatherCard.css'
